6a949085f8565a5246fdf96b3a010832dac0e448
KPiatigorskii <k.piatigorskii@gmail.com>
2023-04-19 13:59:11 +0300

    add

---
+++ app/src/test/java/TestMy.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestMy {
+    @Test
+    void justAnExample() {
+        Assertions.assertTrue(true);
+    }
+}
